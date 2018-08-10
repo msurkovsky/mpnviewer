622fc7751ffc3082745d88c7de1daf1346d3839a
Martin Surkovsky <martin.surkovsky@gmail.com>
2018-08-10 13:00:59 +0200

    ENH: types used within MP nets

--- src/types.tsx
+++ src/types.tsx
@@ -1,4 +1,5 @@
 
+
 export class UID {
 
     public static get(value: any) {
@@ -19,11 +20,40 @@ export class UID {
     }
 }
 
+
+export class Expression {
+
+    private expr: string;
+
+    constructor(expr: string = "") {
+        this.expr = expr;
+    }
+
+    public toString(): string {
+        return this.expr;
+    }
+
+    public isEmpty(): boolean {
+        return this.expr !== "";
+    }
+}
+
+
 export class DataType {
+
+    public static get(name: string): DataType {
+        if (this.types[name] === undefined) {
+            this.types[name] = new DataType(name);
+        }
+
+        return this.types[name];
+    }
+
+    private static types: Map<string, DataType> = new Map();
 
     private name: string;
 
-    constructor (name: string) {
+    private constructor (name: string) {
         this.name = name;
     }
 
@@ -32,15 +62,6 @@ export class DataType {
     }
 }
 
-export class Expression {
-
-    private expr: string;
-
-    constructor(expr: string) {
-        this.expr = expr;
-    }
-
-    public toString(): string {
-        return this.expr;
-    }
-}
+export const mpnUnit = DataType.get("unit");
+export const mpnBoolean = DataType.get("boolean");
+export const mpnInteger = DataType.get("integer");
