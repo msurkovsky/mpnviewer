2740f6bdfa8b3c7ed1cc2eec2aaa16a59f127950
Martin Surkovsky <martin.surkovsky@gmail.com>
2018-08-10 13:01:32 +0200

    RF: index

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 
-import {Net} from './new/net'
-import {Place} from './new/place'
-import {UID} from './new/types'
-/* import {ViewElement} from './netview/netelement'*/
+import {Net, Place} from './netview';
+import {DataType, Expression, UID} from './types';
 
-/* import {Place} from './netmodel/place' */
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
         <Net x={0} y={0} width={1000} height={500}>
-            <Place place={{id: UID.get("a"), name: "a", type: "Bool", initExpr: ""}} bbox={{x: 50, y: 50, width: 40, height: 40}} />
+            <Place
+                place={{
+                    id: UID.get("a"),
+                    name: "a",
+                    type: DataType.get("Bool"),
+                    initExpr: new Expression()}}
+                bbox={{
+                    x: 50,
+                    y: 50,
+                    width: 40,
+                    height: 40}} />
         </Net>,
     document.getElementById('root') as HTMLElement
 );
