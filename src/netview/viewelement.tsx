00c0fe36927a6f0295b70adce56fdd7415918339
Martin Surkovsky <martin.surkovsky@gmail.com>
2018-08-14 16:06:39 +0200

    wip:

--- src/netview/viewelement.tsx
+++ src/netview/viewelement.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
-
-
-interface ViewElementData {
-    bbox: BoundingBox;
-}
+import * as Utils from './utils'
 
 
 export interface BoundingBox {
@@ -14,64 +10,174 @@ export interface BoundingBox {
 }
 
 
-export function createViewElement<T extends {}> (Element: any) {
+// The pros has to coincide with T pros, i.e., 'bounding box' and
+// 'element description' are obligatory.
+interface ViewElementProps<T extends {}> {
+    data: T,
+    bbox: BoundingBox;
+    handlers?: {
+        dataChange?: (data: T) => void;
+        batchMove?: (dx: number, dy: number) => void;
+        unitMove?: (dx: number, dy: number) => void;
+        resize?: (bbox: BoundingBox) => void;
+    };
+    registerMinorBBoxes?: (...bboxes: [string, BoundingBox][]) => void;
+}
 
-    type Props = ViewElementData & T;
-    type State = Props;
 
-    return class extends React.Component<Props, State> {
+interface ViewElementState<T extends {}> {
+    data: T;
+    majorBBox: BoundingBox;
+    minorBBoxes: { [key: string]: BoundingBox };
+}
+
+
+export function createViewElement<T extends {}> (Element: React.ComponentType<ViewElementProps<T> >) {
+
+    type Props = ViewElementProps<T>;
+    type State = ViewElementState<T>;
+
+    return class extends React.PureComponent<Props, State> {
+
+        private startPosition: {x: number, y: number} | null = null;
 
         constructor (props: Props) {
             super(props);
 
-            const {bbox, ...data} = this.props as any; // FIXME: with a new version of typescript remove casting
+            const {bbox, data} = this.props;
+
             this.state = {
-                bbox,   // state of the group's  bounding box
-                ...data // State of the inner object data, modified via the handler;
-                        // view element keeps the sate but know nothing about it.
+                data,
+                majorBBox: bbox,
+                minorBBoxes: {},
             };
         }
 
+        public componentDidMount() { // TODO: I'm not sure whether this is the right place to call it, maybe because it will wait when all the components will be added -> CHECK IT!
+
+            // recompute the bounding box the children elements could change it
+            const pHandleResize = this.props.handlers.resize;
+
+            if (pHandleResize) { // Propagate the change into the parent element,
+                                 // becuase the parent keeps the sate of overall
+                                 // bbox.
+                const bbox = this.computeCurrentBBox();
+                pHandleResize(bbox);
+            }
+        }
+
         public render() {
-            const {bbox, ...data} = this.state as any; // FIXME: the same problem as before
+            const {majorBBox, data} = this.state;
 
             return (
                 <g>
-                    <rect {...bbox} fillOpacity={0} stroke="#000" strokeWidth={1} /> {/* TODO: remove this element; just debug */}
+                    {/* TODO: remove this element; just debug */}
+                    <rect {...majorBBox} fillOpacity={0} stroke="#000" strokeWidth={1} />
                     <Element
-                        {...data}
-                        bbox={bbox}
-                        handleGroupMove={this.handleGroupMove}
+                        data={data}
+                        bbox={majorBBox}
+                        registerMinorBBoxes={this.registerMinorBBoxes}
+                        handleBatchMove={this.handleBatchMove}
+                        handleUnitMove={this.handleUnitMove}
                         handleResize={this.handleResize}
                     />
                 </g>
             );
         }
 
-        public handleGroupMove = (dx: number, dy: number) => {
-            const {bbox, ...data} = this.state as any; // FIXME: get rid of any
-            const {x, y, ...size} = bbox;
+        protected handleBatchMove = (dx: number, dy: number) => {
+            const majorBBox = this.state.majorBBox;
+            const {x, y, ...size} = majorBBox;
 
             this.setState({
-                ...data,
-                bbox: {
+                majorBBox: {
                     x: x + dx,
                     y: y + dy,
                     ...size}
             });
-        }
 
-        public handleIndividualMove = (dx: number, dy: number) => {
+            // TODO: propagate change resize into the parent component
+        };
+
+        protected handleUnitMove = (dx: number, dy: number) => {
+            // TODO: (?) Is there any need to propagate these changes to the parent?
+            //       Unit moves are the business of the current componet
             // nothing yet
+        };
+
+        protected handleResize = (bbox: BoundingBox) => {
+            this.setState({
+                majorBBox: {...bbox}
+            });
+        };
+
+        protected registerMinorBBoxes = (...bboxes: [string, BoundingBox][]) => {
+
+            const minorBBoxes = {...this.state.minorBBoxes};
+            for (const [key, bbox] of bboxes) {
+                minorBBoxes[key] = bbox;
+            }
+            this.setState({ minorBBoxes });
+        };
+
+        protected handleMovingMouseDown = (movingHandler: (e: any) => void) => (e: any) => {
+            this.startPosition = {
+                x: e.pageX,
+                y: e.pageY,
+            };
+            document.addEventListener('mousemove', movingHandler);
+        };
+
+        protected handleMovingMouseUp = (movingHandler: (e: any) => void) => (e: any) => {
+            this.startPosition = null;
+            document.removeEventListener('mousemove', movingHandler);
+        };
+
+        protected handleBatchMouseMoving = (e: MouseEvent) => {
+            if (this.startPosition === null) {
+                return;
+            }
+
+            const dx = e.pageX - this.startPosition.x;
+            const dy = e.pageY - this.startPosition.y;
+
+            this.startPosition.x = e.pageX;
+            this.startPosition.y = e.pageY;
+
+            const pHandleBatchMove = this.props.handleBatchMove;
+            if (pHandleBatchMove) {
+                pHandleBatchMove(dx, dy);
+            }
+        };
+
+        protected handleUnitMouseMoving = (e: MouseEvent) => {
+            if (this.startPosition === null) {
+                return;
+            }
+
+            /* const dx = e.pageX - this.startPosition.x; */
+            /* const dy = e.pageY - this.startPosition.y; */
+
+            this.startPosition.x = e.pageX;
+            this.startPosition.y = e.pageY;
+
+            // TODO: I need somehow identify which bbox should be changed
+
+            /*
+            this.setState({
+                minorBBoxes: ?
+            });
+            */
         }
 
-        public handleResize = (newBbox: BoundingBox) => {
-            const {bbox, ...data} = this.state as any; // FIXME: get rid of any
+        private computeCurrentBBox () {
+            const anchorBBox = this.props.bbox;          // anchor bounding box
+            const {majorBBox, minorBBoxes} = this.state; // boxes realatively positioned to anchor box
 
-            this.setState({
-                ...data,
-                bbox: newBbox
-            });
+            const unifiedBBoxes = Utils.absolutizeBouningBoxes( // TODO: maybe come up with a more clear naming for absolutization, actualy it is a unification of units
+                anchorBBox, [majorBBox].concat(minorBBoxes));
+
+            return Utils.computeBoundingBox(anchorBBox, unifiedBBoxes);
         }
     };
 }
