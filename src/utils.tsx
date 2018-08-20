8a9baa4dcc7cf3e8700d1b5ad48026cfbed7e1df
Martin Surkovsky <martin.surkovsky@gmail.com>
2018-08-20 11:18:41 +0200

    RF: computing default related postions

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,33 +1,51 @@
 import {lensPath, over} from 'ramda'
 import {Net as TNet} from './netmodel'
+import {Size} from './types'
+
+const defaultPositions = {
+    places: (placeSize: Size) => ({
+        "type": {x: placeSize.width, y: placeSize.height},
+        "initExpr": {x: placeSize.width, y: 0},
+    }),
+    transitions: (transSize: Size) => ({
+        guard: {x: 0, y: -5} // TODO: transSize.height + 5 -> align at bottom of transition
+                             //       but have to find out how to propagate (anonymously)
+                             //       other parameters to SVG elements from HOC
+    }),
+};
 
 export const getId = ((id: number) => (): string => {
     return (id++).toString();
 })(0);
 
+
 export function fillDefaultRelatedPositions(net: TNet) {
+
+    const fill = (n: TNet, elem: string) => {
+        for (const key of Object.keys(net[elem])) {
+            const path = lensPath([elem, key]);
+
+            n = over(path, ({size, relatedPositions: defined, ...rest}) => {
+                const relatedPositions = {...defined};
+                const elemDefaultPos = defaultPositions[elem](size);
+
+                for (const p of Object.keys(elemDefaultPos)) {
+                    if (!relatedPositions[p]) {
+                        relatedPositions[p] =  elemDefaultPos[p];
+                    }
+                }
+
+                return {...rest, size, relatedPositions};
+            }, n);
+        }
+
+        return n;
+    };
 
     let newNet: TNet = net;
 
-    for (const key of Object.keys(net.places)) {
-
-        const path = lensPath(["places", key]);
-
-        newNet = over(path, ({data, position, size, relatedPositions: defined}) => {
-            const defaultPositions = { // TODO: put into a common function
-                "type": {x: size.width, y: size.height},
-                "initExpr": {x: size.width, y: 0},
-            };
-
-            const relatedPositions = {...defined};
-            for (const elem of Object.keys(defaultPositions)) {
-                if (!relatedPositions[elem]) { // TODO: use better name then `elem`
-                    relatedPositions[elem] = defaultPositions[elem];
-                }
-            }
-            return {data, position, size, relatedPositions};
-        }, newNet);
-    }
-
+    newNet = fill(newNet, "places");
+    newNet = fill(newNet, "transitions");
     return newNet;
 };
+
