9ee2e20ff7b5a7df9cc5942a312b0c1672a12bfa
Martin Surkovsky <martin.surkovsky@gmail.com>
2018-09-30 21:55:23 +0200

    ENH: added possibility to move with arc's inner points

--- src/netview/arc.tsx
+++ src/netview/arc.tsx
@@ -1,29 +1,27 @@
 import * as React from 'react';
 import * as Utils from '../utils';
 
-import {PositionChanged} from '../events';
+/* import {PositionChanged} from '../events';*/
 
 import {ArcData} from '../netmodel';
-import {Dict, ID, Path, Position} from '../types';
+import {Dict, Omit, Position} from '../types';
 import {font} from '../visualsetting';
+
+import {onMouseDown, onMouseUp, PositionableProps} from './positionable'
 import {TextElement} from './textelement';
 
 export type ArcPositions = Dict<Position> & {
     expression: Position;
 }
 
-interface Props {
-    canvasId: ID;
+type Props = Omit<PositionableProps, "position"> & { // NOTE: The position is
+                                                     // added separtely for each
+                                                     // point.
     data: ArcData;
-    path: Path;
-    zoom: number;
-    pan: Position;
-    anchorPosition: Position;
     points: Array<{x: number, y: number}>;
     relatedPositions: ArcPositions;
     select: () => void;
     remove: () => void;
-    changePosition: (evt: PositionChanged) => void;
 }
 
 export class Arc extends React.PureComponent<Props> {
@@ -34,6 +32,24 @@ export class Arc extends React.PureComponent<Props> {
             changePosition} = this.props;
 
         const anchoredPoints = points.map(p => Utils.v2dAdd(anchorPosition, p));
+
+        const circles = [];
+        for (let i = 1; i < anchoredPoints.length - 1; i++) {
+            const p = anchoredPoints[i];
+            // draggable points binding move features, in order to change
+            // the position of inner points
+            const circle = <circle
+                key={`${arc.id}-${i}`}
+                cx={p.x}
+                cy={p.y}
+                r={7}
+                onMouseDown={this.startMoveInnerPoint(i)}
+                onMouseUp={onMouseUp}
+                fillOpacity={0.0}
+            />
+            circles.push(circle);
+        }
+
         return (
             <g>
             <polyline
@@ -46,6 +62,7 @@ export class Arc extends React.PureComponent<Props> {
                 markerEnd={`url(#${arc.type})`}
                 onClick={this.onClick}
             />
+            {circles}
 
             <TextElement
                 canvasId={canvasId}
@@ -67,5 +84,22 @@ export class Arc extends React.PureComponent<Props> {
         select();
 
         evt.stopPropagation();
+    }
+
+    private startMoveInnerPoint = (idx: number) => (evt: React.MouseEvent) => {
+        const {canvasId, path, zoom, pan, anchorPosition, changePosition, points} = this.props;
+        const position = points[idx];
+        const props = {canvasId, path, zoom, pan, anchorPosition, position, changePosition};
+
+        const setInnerPoint = (oldPoints: Position[], changingIdx: number) =>
+                (newPosition: Position) => {
+            const newPoints = [...points];
+            newPoints[changingIdx]=newPosition;
+
+            // strip first and last point, these are NOT inner ones.
+            return newPoints.slice(1, newPoints.length - 1);
+        };
+
+        onMouseDown(props, ["innerPoints"], setInnerPoint(points, idx))(evt);
     }
 }
